d9216a37f1c06f548065ae8b39230b2dc19256c4
David Nishikawa <triptype@gmail.com>
2017-10-13 10:19:32 -0700

    write red test for get request to '/proteins'

--- test/app.test.js
+++ test/app.test.js
@@ -3,3 +3,25 @@ const request = require('./request');
 const {assert} = require('chai');
 
 
+describe('Protein database api', () => {
+
+    // FUNCTIONS IN IN OTHER FILES:
+    // --------------------------------
+    // - db.js
+    //   - BEFORE: mongodb connect
+    //   - AFTER: mongodb close
+    // 
+    // - request.js
+    //   - AFTER: server close
+
+    describe('get method', () => {
+        it('returns all items in database as an array', () => {
+            return request.get('/proteins')
+                .then(output => {
+                    assert.ok(output instanceof Array);
+                    console.log(output);
+                })
+        });
+    });
+
+})
