0a7b661cf115776841bf8046c00ea6a8e7555af9
ckrountree <jmail0207@gmail.com>
2017-10-13 10:31:04 -0700

    add eslint disable to lines in server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const app = require('./lib/app');
+// const app = require('./lib/app');
 const mongodb = require('./lib/mongodb');
 
 const url = 'mongodb:// localhost:27017/';
@@ -9,6 +9,7 @@ mongodb.connect(url)
     // eslint-disable-next-line
     .catch (err => console.log('mongo FAIL', err));
 
+// eslint-disable-next-line
 const server = http.createServer(app);
 const port = process.env.port || 3000;
 
