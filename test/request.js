08c7088b205a4ba3539da98843e195a588226679
ckrountree <jmail0207@gmail.com>
2017-10-13 10:32:00 -0700

    disable eslint in request.js

--- test/request.js
+++ test/request.js
@@ -1,6 +1,7 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
+// eslint-disable-next-line
 const {assert} = chai;
 const http = require('http');
 const app = require('../lib/app');
