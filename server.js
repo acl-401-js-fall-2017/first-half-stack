fd4012d66359a2fb422617ec372d019e8891e711
mugworks <greenwood.pdx@gmail.com>
2017-10-15 14:48:58 -0700

    template setup and server created

---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const mongodb = require('./lib.mongodb');
+const app = require('app');
+
+const url = 'mongodb://localhost:27017/states';
+mongodb.connect(url)
+    .then(() => console.log('mongo connected', url))
+    .catch(err => console.log('mongo FAIL', err));
+
+const server = http.createServer(app);
+const port = process.env.port || 3000;
+
+server.listen(port, () => {
+    console.log('server running on ', server.address().port);
+});
