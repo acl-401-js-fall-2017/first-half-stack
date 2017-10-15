15070d41fe0c72a61e75ecdc2f0cd438c113a72c
Robin Miller <rmillerpdx@gmail.com>
2017-10-15 14:35:46 -0700

    pass first test

--- test/app-test.js
+++ test/app-test.js
@@ -4,22 +4,28 @@ const assert = chai.assert;
 chai.use(chaiHttp);
 //const http = require('http');
 const mongodb = require('../lib/mongodb');
-const mongoUrl = 'mongodb://localhost:27017/mountains';
+const mongoUrl = 'mongodb://localhost:27017/test';
 
 // const app = require('../lib/app');
 // const server = http.createServer(app);
 // const request = chai.request(server);
 
 describe('MountainsDB',() => {
-
     
-    it('get by id', () => {
+    it('detect existing entry in database', () => {
         return mongodb.connect(mongoUrl)
             .then( results => {
-                assert.ok(results);
+                return results.collection('mountains')
+                    .find({name: 'adams'})
+                    .toArray();
+            })
+            .then( (mountain) => {
+                console.log('mountain is', mountain);
+                assert.deepEqual(mountain, [{
+                    '_id' : 1.0,
+                    'name' : 'adams'
+                }]);
             });
     });
-
-
 
 });
