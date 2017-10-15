aaf9d3d08f4409eb564bbe72dca644c71b98218f
erdem koral <koral.erdem@gmail.com>
2017-10-15 16:26:14 -0700

    the first test is passing

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const http = require('http');
-// const app = require('./lib/app');
+const app = require('./lib/app');
 const mongodb = require('./lib/mongodb');
 const url = process.env.MONGODB_URI;
 
@@ -10,9 +10,7 @@ mongodb.connect(url)
     .then(() => console.log('mongo connected', url))    //eslint-disable-line
     .catch((err) => console.log('error detected',err)); //eslint-disable-line
 
-const server = http.createServer((req, res) => {
-    res.end('server.js is executing.');
-});
+const server = http.createServer(app);
 const PORT = process.env.port || 3000;
 
 server.listen(PORT, () => {
--- test/app.test.js
+++ test/app.test.js
@@ -7,42 +7,36 @@ const { assert } = chai;
 const http = require('http');
 const app = require('../lib/app');
 
-const server = http.createServer(app);
-const request = chai.request(server);
-after(() => server.close());
-before(() => mongodb.connect(url));
-after(() => mongodb.db.close());
-// the code above can be modularized
-
 
 const mongodb = require('../lib/mongodb');
 const testUrl = process.env.MONGODB_URI_TEST;
 
-describe('test treeRats API', () =>{
 
-    it('it gets with id', () => {
-        const treeRats = mongodb.db.collection('treeRats-test');
-        const id = req.requested.params.id;
-        treeRats.findOne( {_id: ObjectID(id) })
-            .then(rodent=>{
-                res.end(JSON.parse(rodent))
-            })
-    }
+describe('test treeRats API', () => {
    
+    let server = null;
+    let request = null;
+    before(() => {
+        server = http.createServer(app);
+        request = chai.request(server);
+    });
 
+    before(() => mongodb.connect(testUrl));
+    beforeEach(() => mongodb.db.dropDatabase());
+    after(() => mongodb.db.close());
+    after(() => server.close());
+    // the code above can be modularized
 
-    // it('it saves with id', () => {
-    //     const rodent = { type: 'flying squirrel', name: 'Apache'};
-    //     request.post('/rodents')
-    //         .send(rodent)
-    //         .then(res => {
-    //             const postedRodent = res.body;
-    //             assert.ok(postedRodent._id);
-    //             assert.equal(postedRodent.name, rodent.name);
-    //         });
-    // });
-
-
+    it('it saves with id', () => {
+        const mickeyMouse ={ type: 'mouse', name: 'mickey' };
+        return request.post('/rodents')
+            .send(mickeyMouse)
+            .then(res => {
+                const rodents = res.body;
+                assert.ok(rodents._id, 'Missing Id');
+                assert.equal(rodents.name, mickeyMouse.name);
+            });
+    });
 
 
 });
