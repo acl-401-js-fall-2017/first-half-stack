eda8f4e2112850cbae78ec36d33861618415923b
mugworks <greenwood.pdx@gmail.com>
2017-10-16 09:18:13 -0700

    update travis and pkg.json

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
-const mongodb = require('./lib.mongodb');
-const app = require('app');
+const mongodb = require('./lib/mongodb');
+// const app = require('./lib/app');
 
 const url = 'mongodb://localhost:27017/states';
 mongodb.connect(url)
