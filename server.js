a5dedba9a1ed48c7525a1e9dd5fdabd5dbd201fc
ckrountree <jmail0207@gmail.com>
2017-10-17 13:12:59 -0700

    uncomment  mongodb

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const http = require('http');
 const app = require('./lib/app');
-// const mongodb = require('./lib/mongodb');
+const mongodb = require('./lib/mongodb');
 
-// const url = 'mongodb://localhost:27017/fruits';
-// mongodb.connect(url)
-//     // eslint-disable-next-line
-//     .then(() => console.log('mongo connected', url))
-//     // eslint-disable-next-line
-//     .catch (err => console.log('mongo FAIL', err));
-
+const url = 'mongodb://localhost:27017/fruits';
+mongodb.connect(url)
+    // eslint-disable-next-line
+    .then(() => console.log('mongo connected', url))
+    // eslint-disable-next-line
+    .catch (err => console.log('mongo FAIL', err));
 
 const server = http.createServer(app);
 const port = process.env.port || 3000;
