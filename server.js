d720ceb00cbb0cea0825282419576591032a40e3
Robin Miller <rmillerpdx@gmail.com>
2017-10-17 10:38:26 -0700

    break everything and integrate basic express functionality. can GET a test object

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const http = require('http');
 const app = require('./lib/app');
-const mongodb = require('./lib/mongodb');
+// const mongodb = require('./lib/mongodb');
 
-const url = 'mongodb://localhost:27017/mountains';
-mongodb.connect(url)
-    .then(()=> console.log('mongo connected', url) )
-    .catch((err)=> console.log('mongo failed with:',err));
+// const url = 'mongodb://localhost:27017/mountains';
+// mongodb.connect(url)
+//     .then(()=> console.log('mongo connected', url) )
+//     .catch((err)=> console.log('mongo failed with:',err));
 
 const server = http.createServer(app);
 const port = process.env.port || 3000;
