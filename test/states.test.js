f730c1fdec0e18aa0eb59fb647e3b6d5a5ea816e
mugworks <greenwood.pdx@gmail.com>
2017-10-16 17:34:03 -0700

    fail get 404 if id is not found

--- test/states.test.js
+++ test/states.test.js
@@ -30,7 +30,7 @@ describe ('states API', () => {
             });
     });
 
-    it('saves with id', () => {
+    it.skip('saves with id', () => {
         const oregon = { name: 'Oregon', city: 'Portland', place: 'Powells'};
         return request.post('/states')
             .send(oregon)
@@ -52,6 +52,16 @@ describe ('states API', () => {
             .then(res => {
                 assert.deepEqual(res.body, state);
             });
+    });
+
+    it('GET 404 if id is not found', () => {
+        return request.get('/states/343423')
+            .then(
+                () => { throw new Error('Unexpected successful response'); },
+                err => {
+                    assert.equal(err.status, 404);
+                }
+            );
     });
 
 });
