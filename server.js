b709b8215aa1667c208945f9f761a633729fa4ae
mugworks <greenwood.pdx@gmail.com>
2017-10-17 21:01:13 -0700

    add file not-found.js

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const app = require('./lib/app');
 
 const url = 'mongodb://localhost:27017/vacations';
 mongodb.connect(url)
-    .then(() => console.log('mongo connected', url)) //eslint-disable-line
-    .catch(err => console.log('mongo FAIL', err));  //eslint-disable-line
+    .then(() => console.log('mongo connected', url));
+    .catch(err => console.log('mongo FAIL', err));
 
 const server = http.createServer(app);
 const port = process.env.port || 3000;
 
 server.listen(port, () => {
-    console.log('server running on ', server.address().port);  //eslint-disable-line
+    console.log('server running on ', server.address().port);
 });
