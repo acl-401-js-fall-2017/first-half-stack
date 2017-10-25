f3350e151d74f2f8291caa8aefe492cf7e46f9dd
mirrorapps <eli.speigel@gmail.com>
2017-10-25 16:17:59 -0700

    removed unessecary mongodb stuff

--- server.js
+++ server.js
@@ -1,12 +1,6 @@
 /*eslint no-console: "off"*/
 const http = require('http'); 
 const app = require('./lib/app');
-const mongodb = require('./lib/mongodb');
-
-const url = 'mongodb://localhost:27017/altcoins';
-mongodb.connect(url)
-    .then(() => console.log('mongo connected', url))
-    .catch(err => console.log('mongo FAIL', err));
 
 const server = http.createServer(app);
 const port = process.env.port || 3000;
